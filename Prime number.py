2b711eeb49462b712c408cba0fd56d801f5ebec7
swastika2015 <jmswastika18409@gmail.com>
2026-06-20 13:25:16 +0530

    Add files via upload

---
+++ Prime number.py
@@ -0,0 +1,17 @@
+lower = int(input("Enter the lower number:"))
+upper =  int(input("Enter the upper number:"))
+
+print ("The numbers between ",lower,"and",upper,"are")
+
+for num in range(lower,upper+1):
+    if num>1:
+      for i in range(2,num):
+         if num % i == 0:
+            break
+else:
+    print(num)
+         
+
+ 
+
+
